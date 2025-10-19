4c1eba85dfedb7ebf4a2027a6f6a293687784624
Tsypin Dmitriy <dtsypin@inbox.ru>
2025-10-19 16:41:15 +0300

    lab4

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,15 @@
+SECTION .data ; Начало секции данных
+hello: DB 'Цыпин Дмитрий',10 
+; символ перевода строки
+helloLen: EQU $-hello 
+SECTION .text 
+GLOBAL _start
+_start: 
+mov eax,4 
+mov ebx,1 
+mov ecx,hello 
+mov edx,helloLen 
+int 80h 
+mov eax,1 
+mov ebx,0 
+int 80h 
